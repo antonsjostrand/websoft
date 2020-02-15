18effafc4caa7261312bc772d21cf9695e10b06a
antonsjostrand <anton.sjostrand@hotmail.se>
2020-02-15 15:45:43 +0100

    Added functions and changed how JSON was formatted started on table creation

--- work/s04/a04/route/lotto.js
+++ work/s04/a04/route/lotto.js
@@ -13,13 +13,32 @@ var router  = express.Router();
 //Add a route for the path /lotto
 router.get('/lotto', (req, res) => {
     let lottoRow = lottoDraw();
-    let jsonResponse = formatJsonResponse(lottoRow);
+    let queryString = req.query.row;
+    
+    if (queryString === undefined){
+        let jsonResponse = formatJsonResponse(lottoRow);
 
-    let lotto = {
-        data : jsonResponse,
-    };
+        let lotto = {
+            data : jsonResponse,
+        };
+    
+        res.render("lotto", lotto);
+        
+    }else {
+        queryString = req.query.row.split(",");
+        let numberOfMatches = calculateMatchingNumbers(lottoRow, queryString);
+        let matchingNumbersList = matchingNumbers(lottoRow, queryString);
 
-    res.render("lotto", lotto);
+        let jsonResponse = formatJsonResponse(lottoRow, queryString, numberOfMatches, matchingNumbersList);
+
+        let lotto = {
+            data : jsonResponse,
+        };
+
+        res.render("lotto", lotto);
+
+    }
+
 });
 
 //Route for /lotto-json
@@ -78,16 +97,39 @@ function calculateMatchingNumbers(lottoRow, queryRow){
     return numberOfMatches;
 }
 
-function formatJsonResponse(lottoRow, queryRow = undefined, numberOfMatches = undefined){
+function formatJsonResponse(lottoRow, queryRow = "", numberOfMatches = "", matchingNumbersList = ""){
 
     let returnObject = {
-        "lottoRow" : JSON.stringify(lottoRow),
-        "queryRow" : JSON.stringify(queryRow),
+        "lottoRow" : JSON.parse(JSON.stringify(lottoRow)),
+        "queryRow" : JSON.parse(JSON.stringify(queryRow)),
         "numberOfMatches" : JSON.stringify(numberOfMatches),
-    }
+        "matchingNumbers" : JSON.parse(JSON.stringify(matchingNumbersList)),
+       };
 
     return JSON.parse(JSON.stringify(returnObject));
 
+}
+
+function matchingNumbers(lottoRow, queryRow){
+    let matchingNumbersList = [];
+    let copyOfLottoRow = lottoRow.slice(0);
+    let index = 0;
+
+    for(let i = 0; i < 7; i++){
+        let number = queryRow[i];
+
+        for(let j = 0; j < copyOfLottoRow.length; j++){
+            let lottoNumber = copyOfLottoRow[j];
+
+            if (number == lottoNumber){
+                copyOfLottoRow.splice(j, 1);
+                matchingNumbersList[index++] = lottoNumber;
+                break;
+            }
+        }
+    }
+
+    return matchingNumbersList;
 }
 
 module.exports = router;
