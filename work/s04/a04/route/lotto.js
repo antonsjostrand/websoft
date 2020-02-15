abce8562819d75c481f4802349b14040591d2746
antonsjostrand <anton.sjostrand@hotmail.se>
2020-02-15 12:31:31 +0100

    Added code for grade 4 in assignment 4

--- work/s04/a04/route/lotto.js
+++ work/s04/a04/route/lotto.js
@@ -10,14 +10,33 @@
 var express = require('express');
 var router  = express.Router();
 
-// Add a route for the path /lotto
+//Add a route for the path /lotto
 router.get('/lotto', (req, res) => {
-    let lotto = {};
+    let lottoRow = lottoDraw();
+    let jsonResponse = formatJsonResponse(lottoRow);
 
-    lotto.lottoRow = lottoDraw();
+    let lotto = {
+        data : jsonResponse,
+    };
 
     res.render("lotto", lotto);
 });
+
+//Route for /lotto-json
+router.get('/lotto-json', (req, res) => {
+    let queryString = req.query.row.split(",");
+    let lottoRow = lottoDraw();
+    let numberOfMatches = calculateMatchingNumbers(lottoRow, queryString);
+
+    let jsonResponse = formatJsonResponse(lottoRow, queryString, numberOfMatches);
+
+    let lotto = {
+        data : jsonResponse,
+    };
+
+    res.render("lotto", lotto);
+
+})
 
 function lottoDraw(){
     let lottoRow = [];
@@ -28,14 +47,47 @@ function lottoDraw(){
             i--;
             continue;
         }
+        if (lottoRow.includes(randomNumber)){
+            i--;
+            continue;
+        }
         lottoRow.push(randomNumber);
     }
 
     return lottoRow;
 }
 
-function test(){
-    return "TESTING FUNCTTIONS";
+function calculateMatchingNumbers(lottoRow, queryRow){
+    let numberOfMatches = 0;
+    let copyOfLottoRow = lottoRow.slice(0);
+
+    for(let i = 0; i < 7; i++){
+        let number = queryRow[i];
+
+        for(let j = 0; j < copyOfLottoRow.length; j++){
+            let lottoNumber = copyOfLottoRow[j];
+
+            if (number == lottoNumber){
+                copyOfLottoRow.splice(j, 1);
+                numberOfMatches++;
+                break;
+            }
+        }
+    }
+
+    return numberOfMatches;
+}
+
+function formatJsonResponse(lottoRow, queryRow = undefined, numberOfMatches = undefined){
+
+    let returnObject = {
+        "lottoRow" : JSON.stringify(lottoRow),
+        "queryRow" : JSON.stringify(queryRow),
+        "numberOfMatches" : JSON.stringify(numberOfMatches),
+    }
+
+    return JSON.parse(JSON.stringify(returnObject));
+
 }
 
 module.exports = router;
